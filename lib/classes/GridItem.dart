2595383af529895104fb96a89f340352f0a33dc8
bashayer-BR <bishooo.sh.sh@gmail.com>
2026-05-22 23:30:15 +0300

    GridItem

--- lib/classes/GridItem.dart
+++ lib/classes/GridItem.dart
@@ -5,5 +5,6 @@ class GridItem {
   final String label;
   final String sublabel;
   final List<Color> gradient;
-  const GridItem(this.icon, this.label, this.sublabel, this.gradient);
+  final void Function()? onTap;
+  const GridItem(this.icon, this.label, this.sublabel, this.gradient, {required this.onTap});
 }
