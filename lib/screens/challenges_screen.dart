571dd5a16cc1cd9dfad38b12c9a75af614554187
bashayer-BR <bishooo.sh.sh@gmail.com>
2026-07-29 15:38:43 +0300

    continue in challanges

--- lib/screens/challenges_screen.dart
+++ lib/screens/challenges_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:imaa/core/AppDim.dart';
 import 'package:imaa/core/theme/theme.dart';
 import 'package:imaa/widgets/buildDailyChallenge.dart';
-import 'package:imaa/widgets/buildGroupChallenge.dart';
 import 'package:imaa/widgets/buildSignQuiz.dart';
 import 'package:imaa/widgets/challenge_widgets.dart';
 
